8095279e3c8efa8e83fa5097ddf577bba4338fbc
letrend <simon.trendel@tum.de>
2016-03-04 18:35:09 +0100

    switched do directed graph

--- include/graph.hpp
+++ include/graph.hpp
@@ -15,10 +15,10 @@ namespace graph {
 
 	struct Vertex {
 		uint index;
-		vector<Edge> edges_in, edges_out;
+		vector<Edge> edges;
 	};
 	struct Edge {
-		Vertex *from, *to;
+		Vertex *to;
 	};
 
 	class Graph {
@@ -26,33 +26,21 @@ namespace graph {
 		Graph(uint numberVertices) {
 			addVertex();
 			Edge edge;
-			edge.from = vertices[0]; // first vertex points to itself
-			edge.to = vertices[0];
-			totalNrEdges++;
-			vertices[0]->edges_in.push_back(edge);
-			vertices[0]->index = 0;
-			indexVertex[0] = vertices[0];
-			for (uint i = 1; i < numberVertices; i++) {
+			for (uint i = 0; i < numberVertices-1; i++) {
 				addVertex();
-				edge.from = vertices[i - 1];
-				edge.to = vertices[i];
+				edge.to = vertices[i+1];
 				totalNrEdges++;
-				vertices[i - 1]->edges_out.push_back(edge);
-				vertices[i]->edges_in.push_back(edge);
+				vertices[i]->edges.push_back(edge);
 				indexVertex[i] = vertices[i];
 				vertices[i]->index = i;
 			}
-			edge.from = vertices.back(); // last vertex points to itself
-			edge.to = vertices.back();
-			totalNrEdges++;
-			vertices.back()->edges_out.push_back(edge);
 		}
 
 		void siblings(uint from) {
 			if (indexVertex.find(from) != indexVertex.end()) {
 				Vertex *v = indexVertex.at(from);
 				cout << "vertex " << from << " siblings: \n";
-				for (auto edge:v->edges_out) {
+				for (auto edge:v->edges) {
 					if (edge.to == v)
 						cout << "none";
 					else
@@ -65,9 +53,9 @@ namespace graph {
 		}
 
 		void traverse(Vertex *from) {
-			if (from == from->edges_out[0].to)
+			if (from->edges.empty())
 				return;
-			for (auto edge:from->edges_out) {
+			for (auto edge:from->edges) {
 				cout << "(" << from->index << " -> " << edge.to->index << ")" << endl;
 				traverse(edge.to);
 			}
@@ -75,9 +63,9 @@ namespace graph {
 
 		template<typename F>
 		void traverse(Vertex *from, F f) {
-			if (from == from->edges_out[0].to)
+			if (from == from->edges[0].to)
 				return;
-			for (auto edge:from->edges_out) {
+			for (auto edge:from->edges) {
 				f(from);
 				traverse(edge.to,f);
 			}
@@ -87,11 +75,9 @@ namespace graph {
 			if (indexVertex.find(from) != indexVertex.end() && indexVertex.find(to) != indexVertex.end()) {
 				Vertex *v0 = indexVertex[from], *v1 = indexVertex[to];
 				Edge edge;
-				edge.from = v0;
 				edge.to = v1;
 				totalNrEdges++;
-				v0->edges_out.push_back(edge);
-				v1->edges_in.push_back(edge);
+				v0->edges.push_back(edge);
 			} else {
 				cout << "ERROR: vertex " << from << " or " << to << " does not exist in graph" << endl;
 			}
@@ -110,16 +96,9 @@ namespace graph {
 				totalNrEdges--;
 				Edge edge;
 				vector<Edge>::iterator edge2delete;
-				edge.from = v0;
 				edge.to = v1;
-				if (checkIfEdgeExists(v0->edges_out, edge, edge2delete))
-					v0->edges_out.erase(edge2delete);
-				if (checkIfEdgeExists(v1->edges_out, edge, edge2delete))
-					v1->edges_out.erase(edge2delete);
-				if (checkIfEdgeExists(v0->edges_in, edge, edge2delete))
-					v0->edges_in.erase(edge2delete);
-				if (checkIfEdgeExists(v1->edges_in, edge, edge2delete))
-					v1->edges_in.erase(edge2delete);
+				if (checkIfEdgeExists(v0->edges, edge, edge2delete))
+					v0->edges.erase(edge2delete);
 			} else {
 				cout << "ERROR: vertex " << from << " or " << to << " does not exist in graph" << endl;
 			}
@@ -127,7 +106,7 @@ namespace graph {
 
 		inline bool checkIfEdgeExists(vector<Edge> &a, Edge &b, vector<Edge>::iterator &edge) {
 			for (auto it = a.begin(); it != a.end(); ++it) {
-				if (it->to->index == b.to->index && it->from->index == b.from->index) {
+				if (it->to->index == b.to->index) {
 					edge = it;
 					return true;
 				}
@@ -135,83 +114,32 @@ namespace graph {
 			return false;
 		}
 
-		void removeVertex(uint v) {
-			auto it = indexVertex.find(v);
-			if (it != indexVertex.end()) {
-				Vertex *v0 = indexVertex[v];
-				// connect all vertices connected by the vertex to be deleted
-				for (auto from:v0->edges_in) {
-					for (auto to:v0->edges_out) {
-						addEdge(from.from->index, to.to->index);
-					}
-				}
-				// delete all edges to siblings and from parents
-				for (auto from:v0->edges_in) {
-					removeEdge(from.from->index, v0->index);
-				}
-				for (auto to:v0->edges_out) {
-					removeEdge(v0->index, to.to->index);
-				}
-				// add edge to itself if it is the first or last vertex
-				if (v0->edges_in.empty() || v0->edges_out.empty()) {
-					addEdge(v0->index, v0->index);
-				}
-				// erase vertex
-				indexVertex.erase(it);
-				for (auto it = vertices.begin(); it != vertices.end(); ++it) {
-					if ((*it)->index == v) {
-						// clear the data
-						delete (*it);
-						vertices.erase(it);
-						return;
-					}
-				}
-			} else {
-				cout << "ERROR: vertex " << v << " does not exist in graph" << endl;
-			}
-		}
-
 		void mergeVertices(uint v, uint w){
 			if (indexVertex.find(v) != indexVertex.end() && indexVertex.find(w) != indexVertex.end()) {
 				Vertex *v0 = indexVertex.at(v), *v1 = indexVertex.at(w);
 				// delete all edges between v0 and v1
 				vector<Edge> edges2delete;
-				for (auto edge:v0->edges_out) {
+				for (auto edge:v0->edges) {
 					if(edge.to==v1) {
 						edges2delete.push_back(edge);
 					}
 				}
 				for(auto edge:edges2delete){
 					vector<Edge>::iterator edge2delete;
-					if (checkIfEdgeExists(v0->edges_out, edge, edge2delete))
-						v0->edges_out.erase(edge2delete);
-				}
-
-				for (auto edge:v1->edges_in) {
-					// adjust all parent nodes to be connected to v0
-					for (uint i=0;i<edge.from->edges_out.size();i++) {
-						if(edge.from->edges_out[i].to == v1)
-							edge.from->edges_out[i].to = v0;
-					}
-					// copy all edges_in to v0
-					if(edge.from!=v0) {
-						edge.to = v0;
-						v0->edges_in.push_back(edge);
-					}
+					if (checkIfEdgeExists(v0->edges, edge, edge2delete))
+						v0->edges.erase(edge2delete);
 				}
 				// copy all edges_out to v0
-				for (auto edge:v1->edges_out) {
+				for (auto edge:v1->edges) {
 					if(edge.to!=v0) {
-						edge.from = v0;
-						v0->edges_out.push_back(edge);
+						v0->edges.push_back(edge);
 					}
 				}
 				// if it is the last vertex terminate it
-				if(v0->edges_out.empty()) {
+				if(v0->edges.empty()) {
 					Edge edge;
-					edge.from = v0;
 					edge.to = v0;
-					v0->edges_out.push_back(edge);
+					v0->edges.push_back(edge);
 				}
 				// erase vertex w
 				auto it = indexVertex.find(w);
@@ -243,8 +171,8 @@ namespace graph {
 			str << "Graph g{\n";
 			for(auto it:vertices){
 				str << it->index << ";\n";
-				for(auto edge:it->edges_out){
-					str << edge.from->index << "--" << edge.to->index << ";\n";
+				for(auto edge:it->edges){
+					str << it->index << "--" << edge.to->index << ";\n";
 				}
 			}
 			str << "}" << endl;
--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@ int main( void )
 	cout << "construct graph" << endl;
     Graph graph(6);
     graph.siblings(0);
-
 	cout << "choose vertex 0" << endl;
 	Vertex* v = graph[0];
 	cout << "traverse graph until end" << endl;
 	graph.traverse(v);
+	graph.graphviz("/home/letrend/workspace/graph/graph.png");
 
 	cout << "add edge between vertex 0 and 5" << endl;
 	graph.addEdge(0,5);
@@ -19,10 +19,6 @@ int main( void )
 
 	cout << "remove edge between vertex 0 and 1" << endl;
 	graph.removeEdge(0,1);
-	graph.traverse(v);
-
-	std::cout << "remove vertex 5" << std::endl;
-	graph.removeVertex(5);
 	graph.traverse(v);
 
 	std::cout << "new graph" << std::endl;
@@ -35,16 +31,23 @@ int main( void )
 	graph2.addEdge(5,9);
 	graph2.addEdge(6,9);
 	graph2.addEdge(7,9);
+	graph2.addEdge(3,5);
+	graph2.addEdge(2,5);
+	graph2.addEdge(1,5);
 
 	v = graph2[0];
 	graph2.traverse(v);
 	std::cout << "merge vertex 1 and 2" << std::endl;
-	graph2.graphviz();
+	graph2.graphviz("/home/letrend/workspace/graph/graph_unmerged.png");
 	graph2.mergeVertices(1,2);
-	graph2.traverse(v);
-	graph2.graphviz();
-
-
+	graph2.mergeVertices(1,3);
+	graph2.mergeVertices(1,4);
+	graph2.mergeVertices(1,5);
+	graph2.mergeVertices(1,6);
+	graph2.mergeVertices(1,7);
+	graph2.mergeVertices(1,8);
+	graph2.mergeVertices(0,1);
+	graph2.graphviz("/home/letrend/workspace/graph/graph_merged.png");
 
     return 0;
 }
